a796d8623929ca41af8fb4a4b2c4aa592e14b69a
alfonsserra <alfonsoserra@gmail.com>
2017-12-19 14:37:47 +0100

    Two way data binding for userName and password

--- src/app/systelab-login/login.component.ts
+++ src/app/systelab-login/login.component.ts
@@ -2,21 +2,38 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
 import { DialogService } from 'systelab-components/widgets/modal/dialog/dialog.service';
 
-export class UserCredendials {
-	constructor(public userName, public password) {
-
-	}
-}
-
 @Component({
 	selector:    'systelab-login',
 	templateUrl: 'login.component.html',
 	styleUrls:   ['login.component.scss']
 })
 export class LoginComponent {
+	private _userName = '';
+	private _password = '';
 
-	public userName = '';
-	public password = '';
+	@Input()
+	get userName(): string {
+		return this._userName;
+	}
+
+	@Output() userNameChange = new EventEmitter();
+
+	set userName(value: string) {
+		this._userName = value;
+		this.userNameChange.emit(this._userName);
+	}
+
+	@Input()
+	get password(): string {
+		return this._password;
+	}
+
+	@Output() passwordChange = new EventEmitter();
+
+	set password(value: string) {
+		this._password = value;
+		this.passwordChange.emit(this._password);
+	}
 
 	@Input() applicationName = undefined;
 	@Input() moduleName = undefined;
@@ -31,13 +48,12 @@ export class LoginComponent {
 	@Input() errorMessage = 'Error';
 	@Input() isLoading = true;
 
-	@Output() login = new EventEmitter<UserCredendials>();
+	@Output() login = new EventEmitter();
 
 	constructor(protected dialogService: DialogService, protected i18nService: I18nService) {
 	}
 
 	public doLogin() {
-		const credentials = new UserCredendials(this.userName, this.password);
-		this.login.emit(credentials);
+		this.login.emit();
 	}
 }
