9da764827315a58455a93f12e6fc5772b7c08224
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-02-04 17:46:30 +0100

    Added unit test that checks if the application is set up successfully

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,46 @@
+import { async, TestBed } from '@angular/core/testing';
+
+import { AppComponent } from './app.component';
+import { SystelabTranslateModule } from 'systelab-translate';
+import { HttpClientModule } from '@angular/common/http';
+import { SystelabLoginModule } from './systelab-login/systelab-login.module';
+import { SystelabComponentsModule } from 'systelab-components';
+import { SystelabPreferencesModule } from 'systelab-preferences';
+import { MessagePopupService } from 'systelab-components/widgets/modal/message-popup/message-popup.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { DialogService } from 'systelab-components/widgets/modal/dialog/dialog.service';
+import { FormsModule } from '@angular/forms';
+import { ShowcaseModule } from './showcase/showcase.module';
+
+
+describe('AppComponent', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				AppComponent
+			],
+			imports: [
+				BrowserModule,
+				FormsModule,
+                HttpClientModule,
+                ShowcaseModule,
+				SystelabLoginModule.forRoot(),
+				SystelabComponentsModule.forRoot(),
+				SystelabTranslateModule.forRoot(),
+				SystelabPreferencesModule.forRoot()
+			],
+			providers: [
+				MessagePopupService,
+				DialogService
+			],
+		})
+			.compileComponents();
+	}));
+
+	it('should create the app', async(() => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app)
+			.toBeTruthy();
+	}));
+});
