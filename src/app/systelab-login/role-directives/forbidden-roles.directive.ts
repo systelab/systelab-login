be3429d3e07911440fd44ac2e58f1751b8700aee
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-02-04 17:26:58 +0100

    Added role based directives

---
+++ src/app/systelab-login/role-directives/forbidden-roles.directive.ts
@@ -0,0 +1,71 @@
+import { Directive, Input, Inject, OnDestroy, OnInit, TemplateRef, ViewContainerRef, trigger } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil, first } from 'rxjs/operators';
+
+import { LoggedUserRolesService } from './logged-user-roles.service';
+
+
+@Directive({
+  selector: '[sltForbiddenRoles]'
+})
+export class ForbiddenRolesDirective implements OnInit, OnDestroy
+{
+    @Input() sltForbiddenRoles: string[];
+    isVisible: boolean = false;
+    stop$ = new Subject();
+
+    constructor(
+        private viewContainerRef: ViewContainerRef,
+        private templateRef: TemplateRef<any>,
+        private loggedUserRolesService: LoggedUserRolesService
+    )
+    {}
+
+    ngOnInit()
+    {
+        this.loggedUserRolesService.getLoggedUserRoles().pipe(takeUntil(this.stop$)).subscribe(
+            (loggedUserRoles: string[]) =>
+            {
+                if (!loggedUserRoles)
+                {
+                    this.viewContainerRef.clear();
+                }
+
+                const isLogged: boolean = loggedUserRoles && (loggedUserRoles.length > 0);
+                if (!isLogged || this.isAnyRoleForbidden(loggedUserRoles))
+                {
+                    this.isVisible = false;
+                    this.viewContainerRef.clear();
+                }
+                else
+                {
+                    if (!this.isVisible)
+                    {
+                        this.isVisible = true;
+                        this.viewContainerRef.createEmbeddedView(this.templateRef);
+                    }
+                }
+            }
+        );
+    }
+
+    ngOnDestroy()
+    {
+        this.stop$.next();
+    }
+
+    private isAnyRoleForbidden(roles: string[]): boolean 
+    {
+        return !!roles.find(
+            (role: string) :boolean =>
+            {
+                return !!this.sltForbiddenRoles.find(
+                    (forbiddenRole: string) :boolean =>
+                    {
+                        return role === forbiddenRole;
+                    }
+                );
+            }
+        );
+    }
+}
---
+++ src/app/systelab-login/role-directives/logged-user-roles.service.ts
@@ -0,0 +1,9 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export abstract class LoggedUserRolesService
+{
+    public abstract getLoggedUserRoles(): Observable<string[]>;
+}
