8476dc99ab5ff14766541f477b02835a50f74d9a
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-02-04 17:46:08 +0100

    Added show case for the new role based directives

---
+++ src/app/showcase/showcase-role-directives/showcase-logged-users-can-view.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showcase-logged-users-can-view',
+  template: `
+      <div>Any logged user can see this</div>
+  `,
+  styles: [`
+    div {
+      width: 300px;
+      border: 2px solid black;
+      margin-bottom: 5px;
+      padding: 4px;
+    }
+  `]
+})
+export class ShowcaseLoggedUsersCanViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
