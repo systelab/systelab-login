4612a8d8e46ca47b6c31ac6f7721193d259a5eee
alfonsserra <alfonsoserra@gmail.com>
2019-02-11 15:46:52 +0100

    Update systelab-components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,55 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { DialogService } from 'systelab-components/widgets/modal/dialog/dialog.service';
+import { Component } from '@angular/core';
+import { DialogService } from 'systelab-components/widgets/modal';
 import { ChangePasswordDialog } from './systelab-login/change-password-dialog.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['app.component.scss'],
-    encapsulation: ViewEncapsulation.None
+	selector:    'app-root',
+	templateUrl: './app.component.html'
 })
 export class AppComponent {
 
-    public userName = '';
-    public password = '';
-    public name = '';
-    public lastName = '';
-    public currentForm = '';
-    public email = '';
-    public errorUserPwd = false;
-    public errorUserDoesNotExist = false;
-    public errorUserExist = false;
-    public txtUsername = '';
-    public txtRecoverProcessStarted = '';
-    public pathTerms = 'http://www.werfen.com/en/terms-and-conditions';
-    public pathPrivacy = 'http://www.werfen.com/en/privacy-policy';
-    public isRecoveryActive = true;
-    public isSignUpActive = true;
-    public isLoginActive = true;
-    public isLoading = false;
+	public userName = '';
+	public password = '';
+	public name = '';
+	public lastName = '';
+	public currentForm = '';
+	public email = '';
+	public errorUserPwd = false;
+	public errorUserDoesNotExist = false;
+	public errorUserExist = false;
+	public txtUsername = '';
+	public txtRecoverProcessStarted = '';
+	public pathTerms = 'http://www.werfen.com/en/terms-and-conditions';
+	public pathPrivacy = 'http://www.werfen.com/en/privacy-policy';
+	public isRecoveryActive = true;
+	public isSignUpActive = true;
+	public isLoginActive = true;
+	public isLoading = false;
 
-    public constructor(protected dialogService: DialogService) {
-        this.currentForm = 'login';
-        this.txtUsername = 'Username';
-    }
+	public constructor(protected dialogService: DialogService) {
+		this.currentForm = 'login';
+		this.txtUsername = 'Username';
+	}
 
-    public doLogin() {
-        console.log(this.userName + ' ' + this.password);
-        this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
-        console.log('doLogin');
-        this.isLoading = true;
-    }
+	public doLogin() {
+		console.log(this.userName + ' ' + this.password);
+		this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
+		console.log('doLogin');
+		this.isLoading = true;
+	}
 
-    public doSignUp() {
-        console.log(this.userName + ' ' + this.password + ' ' + this.name + ' ' + this.lastName + ' ' + this.email);
-        this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
-        console.log('doSignUp');
-        this.isLoading = true;
-    }
+	public doSignUp() {
+		console.log(this.userName + ' ' + this.password + ' ' + this.name + ' ' + this.lastName + ' ' + this.email);
+		this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
+		console.log('doSignUp');
+		this.isLoading = true;
+	}
 
-    public doRecovery() {
-        console.log(this.userName);
-        this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
-        console.log('doRecovery');
-        this.isLoading = true;
-    }
+	public doRecovery() {
+		console.log(this.userName);
+		this.dialogService.showDialog(ChangePasswordDialog, ChangePasswordDialog.getParameters());
+		console.log('doRecovery');
+		this.isLoading = true;
+	}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,35 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
-import {SystelabComponentsModule} from 'systelab-components';
-import {SystelabPreferencesModule} from 'systelab-preferences';
-import {SystelabTranslateModule} from 'systelab-translate';
-import {HttpClientModule} from '@angular/common/http';
-import {SystelabLoginModule} from './systelab-login/systelab-login.module';
-import {ShowcaseModule} from './showcase/showcase.module';
-import {MessagePopupService} from 'systelab-components/widgets/modal/message-popup/message-popup.service';
-import {DialogService} from 'systelab-components/widgets/modal/dialog/dialog.service';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SystelabComponentsModule } from 'systelab-components';
+import { SystelabPreferencesModule } from 'systelab-preferences';
+import { SystelabTranslateModule } from 'systelab-translate';
+import { HttpClientModule } from '@angular/common/http';
+import { SystelabLoginModule } from './systelab-login/systelab-login.module';
+import { ShowcaseModule } from './showcase/showcase.module';
+import { DialogService, MessagePopupService } from 'systelab-components/widgets/modal';
 
 @NgModule({
-    declarations: [
-        AppComponent],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        ShowcaseModule,
-        SystelabLoginModule.forRoot(),
-        SystelabComponentsModule.forRoot(),
-        SystelabTranslateModule.forRoot(),
-        SystelabPreferencesModule.forRoot()
-    ],
-    providers: [
-        MessagePopupService,
-        DialogService
-    ],
-    bootstrap: [AppComponent]
+	declarations: [
+		AppComponent],
+	imports:      [
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
+		ShowcaseModule,
+		SystelabLoginModule.forRoot(),
+		SystelabComponentsModule.forRoot(),
+		SystelabTranslateModule.forRoot(),
+		SystelabPreferencesModule.forRoot()
+	],
+	providers:    [
+		MessagePopupService,
+		DialogService
+	],
+	bootstrap:    [AppComponent]
 })
 export class AppModule {
 }
 
-export {AppComponent} from './app.component';
+export { AppComponent } from './app.component';
